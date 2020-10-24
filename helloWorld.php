21fa7fc33d77f1c4b7febda127d5f88c23a36b92
Sadi-Pritom <sadipritom@gmail.com>
2020-10-25 02:18:09 +0600

    Create helloWorld.php

---
+++ helloWorld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <title>My first application app</title>
+
+   </head>
+<body>
+
+<h1>My first PHP page</h1>
+<?php
+echo "Hello World!";
+?>
+</body>
+</html>
